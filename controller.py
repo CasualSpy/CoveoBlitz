e7a2dff65c3af109aeee3515621e8685f06ec52b
CacttusMan <godinchristo@hotmail.com>
2021-01-15 21:06:20 -0500

    StateMachine

--- controller.py
+++ controller.py
@@ -1,6 +1,12 @@
+from enum import Enum
 from game_message import GameMessage, Crew
 class controller:
     def __init__(self, game_message: GameMessage):
         pass
     def turn(self, game_message: GameMessage, crew: Crew):
         pass
+
+class StateMachine(Enum):
+    EARLYGAME = "EARLYGAME"
+    MIDGAME = "MIDGAME"
+    ENDGAME = "ENDGAME"
