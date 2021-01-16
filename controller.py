e333a0d6c348ff7dbfba5ea47615b3b2c92b7dbf
CacttusMan <godinchristo@hotmail.com>
2021-01-15 21:42:22 -0500

    GetDistanceBetween

--- controller.py
+++ controller.py
@@ -1,10 +1,24 @@
 from enum import Enum
+import math
 from game_message import GameMessage, Crew
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
 class controller:
     def __init__(self, game_message: GameMessage):
         pass
+
     def turn(self, game_message: GameMessage, crew: Crew):
         pass
+
+    def getdistancebetween(self, point1: Point, point2: Point):
+        return math.sqrt((point1.x - point2.x)**2 + (point1.y - point2.y)**2)
+
 
 class StateMachine(Enum):
     EARLYGAME = "EARLYGAME"
